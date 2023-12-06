83a3a4fee38335895f285a6f4008fa82d8b59603
BenTechstarter <benedict@docc.techstarter.de>
2023-12-06 15:57:12 +0100

    install mysql + fixed

--- main.js
+++ main.js
@@ -2,21 +2,47 @@ var mysql = require('mysql');
 const express = require('express');
 const app = express();
 const port = 3000;
+require("dotenv").config();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    console.log("Req:", req);
-    res.send("Hello World");
-})
+var con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
 
-app.post("/hello", (req, res) => {
+// Connection zu mysql
+/*   con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  }); */
+  
+// Connection zu mysql
+  con.connect();
+
+
+// List Route
+app.get("/", (req, res) => {
     console.log("Req:", req.body);
-    res.status(200).send(req.body);
-})
+    // sql query um alles aus dem table tasks aufzurufen
+    con.query("SELECT * FROM tasks;", (err, result) => {
+        if (err) {
+          console.error("Error fetching data:", err);
+          res.status(500).send("Error fetching data");
+      } else {
+        // sql Ergebnis in json umwandeln
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify?retiredLocale=de
+//          var jsonobj = JSON.stringify(result);
+          res.json({ tasks:result })
+          res.status(200).send(jsonobj);
+      }
+    });
+});
 
  // Save Route
-app.post("/save", (req, res) => {
+app.post("/", (req, res) => {
     console.log("Req:", req.body);
     // sql query um Daten zu speichern
     var saveSql = `INSERT INTO tasks (taskname, taskstatus) VALUES (?, ?);`
@@ -31,25 +57,9 @@ app.post("/save", (req, res) => {
     });
 })
 
-// List Route
-app.post("/list", (req, res) => {
-    console.log("Req:", req.body);
-    // sql query um alles aus dem table tasks aufzurufen
-    con.query("SELECT * FROM tasks;", (err, result) => {
-        if (err) {
-          console.error("Error fetching data:", err);
-          res.status(500).send("Error fetching data");
-      } else {
-        // sql Ergebnis in json umwandeln
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify?retiredLocale=de
-          var jsonobj = JSON.stringify(result);
-          res.status(200).send(jsonobj);
-      }
-    });
-});
 
 // Update Route
-app.post("/update", (req, res) => {
+app.put("/", (req, res) => {
     console.log("Req:", req.body);
     const { id } = req.body;
     console.log(id);
@@ -74,7 +84,7 @@ app.post("/update", (req, res) => {
 });
 
 // delete route
-app.post('/delete', (req, res) => {
+app.delete('/', (req, res) => {
   const { id } = req.body;
   const deleteSql = 'DELETE FROM tasks WHERE id = ?;';
   con.query(deleteSql, [id], (err, result) => {
@@ -92,16 +102,3 @@ app.listen(port, () => {
     console.log(`ToDo App started on Port ${port}`);
     //or console.log("ToDo App started on Port" + ${port});
 })
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "ToDoUser",
-    password: "bmns",
-    database: "todoexpress"
-  });
-
-// Connection zu mysql
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
